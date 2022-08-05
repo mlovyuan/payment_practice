cc8b1c1ff3f7bf93baae8f50c146f3461ac4b960
mlovyuan <mlovyuan@yahoo.com.tw>
2022-08-05 23:22:12 +0800

    init project

---
+++ src/main/java/com/payment/PaymentPracticeApplication.java
@@ -0,0 +1,14 @@
+package com.payment;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentPracticeApplication.class, args);
+    }
+
+}
