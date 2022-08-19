35ee0821a8c3cd78304b08cbb26da03dc1ca6f5c
mlovyuan <mlovyuan@yahoo.com.tw>
2022-08-19 16:32:14 +0800

    add swagger congfig and test

--- src/main/java/com/payment/PaymentPracticeApplication.java
+++ src/main/java/com/payment/PaymentPracticeApplication.java
@@ -3,8 +3,9 @@ package com.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class PaymentPracticeApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/payment/controller/ProductController.java
+++ src/main/java/com/payment/controller/ProductController.java
@@ -1,14 +1,18 @@
 package com.payment.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "商品管理")
 @RestController
 @RequestMapping("api/product")
 public class ProductController {
 
     @GetMapping("/test")
+    @ApiOperation("點擊測試")
     public String test() {
         return "test";
     }
